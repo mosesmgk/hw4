76fdd87ac465fe037ab7d0915152c136d2bb2222
EC2 Default User <ec2-user@ip-172-31-42-84.us-east-2.compute.internal>
2020-07-22 01:20:00 +0000

    v1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+var faker = require('faker');
+
+
+//routes 
+app.get("/", function(req, res){
+    res.render("index.html", {"fakerEmail":faker.internet.email(),});
+});
+
+app.get("/phishing", function(req, res){
+    res.render("phishing.html");
+});
+
+app.get("/ransomware", function(req, res){
+    res.render("ransomware.html");
+});
+app.get("/zero", function(req, res){
+    res.render("zero.html");
+});
+
+
+//starting server 
+//app.listen("8080", "127.0.0.1", function(){
+ // console.log("Express server is running...");
+//});
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
